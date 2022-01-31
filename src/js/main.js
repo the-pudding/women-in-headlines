66c3f13ae23f039ebd4ab8f8c637263f2f17cd66
Michelle McGhee <michmac202@gmail.com>
2022-01-31 15:36:55 -0800

    marquee attempt

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 /* global d3 */
-import debounce from 'lodash.debounce';
-import isMobile from './utils/is-mobile';
-import linkFix from './utils/link-fix';
-import modalSetup from './utils/modal-a11y';
-import graphic from './graphicX';
-import footer from './footer';
+import debounce from "lodash.debounce";
+import isMobile from "./utils/is-mobile";
+import linkFix from "./utils/link-fix";
+import modalSetup from "./utils/modal-a11y";
+import graphic from "./graphicX";
+import footer from "./footer";
 
-const $body = d3.select('body');
+const $body = d3.select("body");
 let previousWidth = 0;
 
 function resize() {
@@ -20,72 +20,71 @@ function resize() {
 }
 
 function setupStickyHeader() {
-  const $header = $body.select('header');
-  if ($header.classed('is-sticky')) {
-    const $menu = $body.select('#slide__menu');
-    const $toggle = $body.select('.header__toggle');
+  const $header = $body.select("header");
+  if ($header.classed("is-sticky")) {
+    const $menu = $body.select("#slide__menu");
+    const $toggle = $body.select(".header__toggle");
 
-    modalSetup($toggle, $toggle, $header, $menu, 'a, button, .logo', true);
+    modalSetup($toggle, $toggle, $header, $menu, "a, button, .logo", true);
   }
 }
-var aText = new Array(
-  "When", 
-  "Women",
-  "Make",
-  "Headlines"
-  );
+
+var aText = new Array("When", "Women", "Make", "Headlines");
 
 var iSpeed = 10; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
 var iScrollAt = 4; // start scrolling up at this many lines
-  
+
 var iTextPos = 0; // initialise text position
-var sContents = ''; // initialise contents variable
+var sContents = ""; // initialise contents variable
 var iRow; // initialise current row
 
 var params = {
   iTextPos: 0,
   iIndex: 0,
-  iRow: 0
-}
+  iRow: 0,
+};
 
-function typewriter(params)
-{
+function typewriter(params) {
   //console.log("enter typewriter")
- 
 
-	sContents =  '';
-	
-	iRow = Math.max(0, params.iIndex-iScrollAt);
-	var destination = document.getElementById("typedtext");
-	
-	while ( iRow < params.iIndex ) {
-	//console.log("entered while")
-	sContents += aText[iRow++] + '<br />';
-	}
-	//console.log(params)
-	destination.innerHTML = sContents + aText[params.iIndex].substring(0, params.iTextPos);
+  sContents = "";
+
+  iRow = Math.max(0, params.iIndex - iScrollAt);
+  var destination = document.getElementById("typedtext");
+
+  while (iRow < params.iIndex) {
+    //console.log("entered while")
+    sContents += aText[iRow++] + "<br />";
+  }
+  //console.log(params)
+  destination.innerHTML =
+    sContents + aText[params.iIndex].substring(0, params.iTextPos);
   // word ends here
-	if ( params.iTextPos++ == iArrLength ) {
-	params.iTextPos = 0;
-	params.iIndex++;
-	if ( params.iIndex != aText.length ) {
-	iArrLength = aText[params.iIndex].length;
-	setTimeout(function(){typewriter(params)}, 300);
-	}
-	} else {
-	setTimeout(function(){typewriter(params)}, iSpeed);
-	}
+  if (params.iTextPos++ == iArrLength) {
+    params.iTextPos = 0;
+    params.iIndex++;
+    if (params.iIndex != aText.length) {
+      iArrLength = aText[params.iIndex].length;
+      setTimeout(function () {
+        typewriter(params);
+      }, 300);
+    }
+  } else {
+    setTimeout(function () {
+      typewriter(params);
+    }, iSpeed);
+  }
 }
 
 function init() {
   // adds rel="noopener" to all target="_blank" links
   linkFix();
   // add mobile class to body tag
-  $body.classed('is-mobile', isMobile.any());
+  $body.classed("is-mobile", isMobile.any());
   // setup resize event
-  window.addEventListener('resize', debounce(resize, 150));
+  window.addEventListener("resize", debounce(resize, 150));
   // setup sticky header menu
   setupStickyHeader();
   // kick off graphic code
@@ -93,8 +92,6 @@ function init() {
   // load footer stories
   footer.init();
 
-  
-  
   //console.log("-----",typewriter)
   // typewriter(params);
 }
